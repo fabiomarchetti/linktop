05ce84e62c1885870972a62815edcd340261dedb
fabiomarchetti <marchettisoft@gmail.com>
2026-04-05 17:15:06 +0200

    aggiornamento flutter4

--- app/supervisione/[id]/HealthChartModal.tsx
+++ app/supervisione/[id]/HealthChartModal.tsx
@@ -103,7 +103,8 @@ export default function HealthChartModal({
 
   return (
     <div
-      className="fixed inset-0 z-50 bg-black/60 flex items-center justify-center p-4"
+      className="fixed inset-0 bg-black/60 flex items-center justify-center p-4"
+      style={{ zIndex: 9999 }}
       onClick={onClose}
     >
       <div
